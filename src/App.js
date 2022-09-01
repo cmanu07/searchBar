0d31271577d72a8a3ca083ec408a7adf6ad2add8
cmanu07 <cemanuel86@gmail.com>
2022-09-01 15:58:55 +0300

    third update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { dataSet } from "./constants";
 const App = () => {
 
   return (<>
-          <SearchBar data={dataSet}/>
+          <SearchBar data={dataSet} />
         </>)
 }
 
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,22 +4,28 @@ import { useState } from 'react';
 import searchIcon from './images/icons8-search-50.png'
 
 
-export default function SearchBar ({data}) {
+export default function SearchBar ({data, key}) {
 
     const [cauta, setCauta] = useState([]);
 
     const filtruCautare = (event) => {
         const cuvImput = event.target.value;
         const newFiltru = data.filter((x) => {
-            return x.includes(cuvImput);
+            return x.toLowerCase().includes(cuvImput.toLowerCase());
         })
         setCauta(newFiltru);
     }
 
     return (<main className="main">
                 <label><input type="search" placeholder='Search here...' onChange={filtruCautare}/><img src={searchIcon} alt="..."/></label>
-                <div>{cauta.map((inreg) => {
-                    return <p>{inreg}</p>
-                })}</div>
+                {
+                    (cauta.length === 0) ? 
+                        <div>{data.map((inreg, index) => {
+                            return <p key={index}>{inreg}</p>
+                        })}</div>         :
+                        <div>{cauta.map((inreg, index) => {
+                            return <p key={index}>{inreg}</p>
+                        })}</div>           
+                }
             </main>)
 }
