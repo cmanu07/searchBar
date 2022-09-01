d99331b0c0a479abc30c79f2c0ec152ab2a59250
cmanu07 <cemanuel86@gmail.com>
2022-09-01 16:52:59 +0300

    third update

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,28 +4,31 @@ import { useState } from 'react';
 import searchIcon from './images/icons8-search-50.png'
 
 
-export default function SearchBar ({data, key}) {
+export default function SearchBar ({data}) {
 
     const [cauta, setCauta] = useState([]);
-
+    
     const filtruCautare = (event) => {
         const cuvImput = event.target.value;
         const newFiltru = data.filter((x) => {
-            return x.toLowerCase().includes(cuvImput.toLowerCase());
+            let rez = x.toLowerCase().includes(cuvImput.toLowerCase());
+            return rez;
         })
+        // aici am vrut sa afisez un mesaj...daca nu gaseste nici o cautare
+        // la mine daca nu mi gaseste nici o cautare imi afiseaza intreaga lista
         setCauta(newFiltru);
     }
 
     return (<main className="main">
                 <label><input type="search" placeholder='Search here...' onChange={filtruCautare}/><img src={searchIcon} alt="..."/></label>
                 {
-                    (cauta.length === 0) ? 
-                        <div>{data.map((inreg, index) => {
-                            return <p key={index}>{inreg}</p>
-                        })}</div>         :
+                    (cauta.length !== 0) ? 
                         <div>{cauta.map((inreg, index) => {
                             return <p key={index}>{inreg}</p>
-                        })}</div>           
+                        })}</div>        :
+                        <div>{data.map((inreg, index) => {
+                            return <p key={index}>{inreg}</p>
+                        })}</div>
                 }
             </main>)
 }
